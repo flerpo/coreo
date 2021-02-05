56cbec21729b3cc01721e44a3a0444d184f5006f
Alex Råsten <alex.rasten@gmail.com>
2021-02-05 15:24:02 +0100

    sbubscribe and publish kafka

---
+++ Coreo/Controllers/TrackController.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coreo.Controllers
+{
+    public class TrackController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            throw new Exception("meep");
+        }
+    }
+}
--- Coreo/Program.cs
+++ Coreo/Program.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using Confluent.Kafka;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Coreo
 {
@@ -13,11 +10,21 @@ namespace Coreo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ConnectToKafka();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        private static void ConnectToKafka()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = "moop",
+            };
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            producer.Produce(
+                "meep", new Message<Null, string> { Value = "hello rami" });
+            producer.Flush();
+        }
+       
     }
 }
